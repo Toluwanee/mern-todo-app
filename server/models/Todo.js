355f966929c695e8396644e291ebd33eb2809126
Toluwanee <toluwalope9@gmail.com>
2025-05-26 13:54:56 +0100

    Todo items schemas defined

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -4,6 +4,23 @@ import mongoose from "mongoose";
 
 const TodoSchema = new mongoose.Schema({
     task: {
-        
-    }
-})
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+
+export default Todo;
