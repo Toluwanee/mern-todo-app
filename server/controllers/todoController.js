30fe6c51f3d23c6314de7730beb2c607d9cd3e6c
Toluwanee <toluwalope9@gmail.com>
2025-05-26 15:00:05 +0100

    create todo logic added to the controller

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -12,3 +12,21 @@ export const getTodos = async (req, res) =>  {
     }
 };
 
+// Create a new todo
+export const createTodo = async (req, res) => {
+    const { task } = req.body; //  extract data from request body
+
+    // Basic validation
+  if (!task || task.trim().length < 3) {
+    return res.status(400).json({ message: 'Task is required and must be at least 3 characters long.' }); // 400 Bad Request
+  }
+
+  const newTodo = new Todo({ task });
+  
+  try {
+    const savedTodo = await newTodo.save();
+    res.status(201).json(savedTodo);
+  } catch (error) {
+    res.status(400).json({ message: error.message }); // 400 Bad Request if validation fails (e.g., minlength)
+  }
+}
